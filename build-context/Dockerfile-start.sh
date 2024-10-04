9e583bbfe01f5494b3c13501e5ff70a01fe45a7a
Aliuken <alber84ou@gmail.com>
2024-10-04 13:45:59 +0200

    a

--- build-context/Dockerfile-start.sh
+++ build-context/Dockerfile-start.sh
@@ -1,4 +1,4 @@
 sudo docker build -t job-vacancies-app -f docker-compose-app/Dockerfile .
 sudo docker network create external-net-app
 
-sudo docker run -d --name tomcat-app --network external-net-app -p 9080:8080 -v /AppData_Java17/JobVacanciesApp:/AppData_Java17/JobVacanciesApp job-vacancies-app
+sudo docker container run -d --name tomcat-app --network external-net-app -p 9080:8080 -v /AppData_Java17/JobVacanciesApp:/AppData_Java17/JobVacanciesApp job-vacancies-app
