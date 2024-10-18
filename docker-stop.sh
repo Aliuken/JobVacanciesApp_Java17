0721268d3af517359d90490049421d1b23a8405f
Aliuken <alber84ou@gmail.com>
2024-10-18 16:53:59 +0200

    a

--- docker-stop.sh
+++ docker-stop.sh
@@ -4,13 +4,13 @@ sudo ./docker-compose-stop.sh
 cd ../docker-compose-elk
 sudo ./docker-compose-stop.sh
 
-sudo docker volume rm $(docker volume ls -q)
+sudo docker volume rm $(sudo docker volume ls -q)
 sudo docker volume prune -f
 sudo docker network rm "external-net-app"
 sudo docker network rm "external-net-elk"
 
-sudo docker kill $(docker ps -q)
-sudo docker rm -f -v $(docker ps -a -q)
-sudo docker rmi -f $(docker images -q)
+sudo docker kill $(sudo docker ps -q)
+sudo docker rm -f -v $(sudo docker ps -a -q)
+sudo docker rmi -f $(sudo docker images -q)
 
 sudo docker system prune -a -f --volumes
