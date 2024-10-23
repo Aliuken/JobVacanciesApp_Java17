0a36c2ebfb594326d3d8e94bb61d9ce327050ae0
Aliuken <alber84ou@gmail.com>
2024-10-23 14:22:45 +0200

    a

--- src/main/java/com/aliuken/jobvacanciesapp/model/dto/EndpointRegexPatternDTO.java
+++ src/main/java/com/aliuken/jobvacanciesapp/model/dto/EndpointRegexPatternDTO.java
@@ -44,10 +44,10 @@ public record EndpointRegexPatternDTO(
 		return endpointRegexPatternDTO;
 	}
 
-	public boolean matches(final String httpMethodNameToMatch, final String pathRegexToMatch) {
+	public boolean matches(final String httpMethodNameToMatch, final String pathToMatch) {
 		final boolean result;
 		if(httpMethodName.equals(httpMethodNameToMatch)) {
-			final Matcher matcher = pathRegexPattern.matcher(pathRegexToMatch);
+			final Matcher matcher = pathRegexPattern.matcher(pathToMatch);
 			result = matcher.matches();
 		} else {
 			result = false;
