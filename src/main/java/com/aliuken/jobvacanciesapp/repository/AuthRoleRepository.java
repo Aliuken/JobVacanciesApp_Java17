9ed451de6b094f9e08d9dc7f679cfded4d51ed85
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-03-23 15:44:52 +0100

    first commit

---
+++ src/main/java/com/aliuken/jobvacanciesapp/repository/AuthRoleRepository.java
@@ -0,0 +1,40 @@
+package com.aliuken.jobvacanciesapp.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.aliuken.jobvacanciesapp.annotation.RepositoryMethod;
+import com.aliuken.jobvacanciesapp.model.entity.AuthRole;
+import com.aliuken.jobvacanciesapp.model.entity.factory.AuthRoleFactory;
+import com.aliuken.jobvacanciesapp.model.entity.factory.superclass.AbstractEntityFactory;
+import com.aliuken.jobvacanciesapp.repository.superinterface.UpgradedJpaRepository;
+
+@Repository
+public interface AuthRoleRepository extends UpgradedJpaRepository<AuthRole> {
+	public static final AbstractEntityFactory<AuthRole> ENTITY_FACTORY = new AuthRoleFactory();
+
+//	@RepositoryMethod
+//	@Query("SELECT ar FROM AuthRole ar WHERE ar.name = :name")
+//	public abstract AuthRole findByName(@Param("name") String name);
+
+	@RepositoryMethod
+	public default AuthRole findByName(final String name) {
+		if(name == null) {
+			return null;
+		}
+
+		final Map<String, Object> parameterMap = new HashMap<>();
+		parameterMap.put("name", name);
+
+		final AuthRole authRole = this.executeQuerySingleResult(
+			"SELECT ar FROM AuthRole ar WHERE ar.name = :name", parameterMap);
+		return authRole;
+	}
+
+	@Override
+	public default AbstractEntityFactory<AuthRole> getEntityFactory() {
+		return ENTITY_FACTORY;
+	}
+}
---
+++ src/main/java/com/aliuken/jobvacanciesapp/repository/JobCategoryRepository.java
@@ -0,0 +1,40 @@
+package com.aliuken.jobvacanciesapp.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.aliuken.jobvacanciesapp.annotation.RepositoryMethod;
+import com.aliuken.jobvacanciesapp.model.entity.JobCategory;
+import com.aliuken.jobvacanciesapp.model.entity.factory.JobCategoryFactory;
+import com.aliuken.jobvacanciesapp.model.entity.factory.superclass.AbstractEntityFactory;
+import com.aliuken.jobvacanciesapp.repository.superinterface.UpgradedJpaRepository;
+
+@Repository
+public interface JobCategoryRepository extends UpgradedJpaRepository<JobCategory> {
+	public static final AbstractEntityFactory<JobCategory> ENTITY_FACTORY = new JobCategoryFactory();
+
+//	@RepositoryMethod
+//	@Query("SELECT jc FROM JobCategory jc WHERE jc.name = :name")
+//	public abstract JobCategory findByName(@Param("name") String name);
+
+	@RepositoryMethod
+	public default JobCategory findByName(final String name) {
+		if(name == null) {
+			return null;
+		}
+
+		final Map<String, Object> parameterMap = new HashMap<>();
+		parameterMap.put("name", name);
+
+		final JobCategory jobCategory = this.executeQuerySingleResult(
+			"SELECT jc FROM JobCategory jc WHERE jc.name = :name", parameterMap);
+		return jobCategory;
+	}
+
+	@Override
+	public default AbstractEntityFactory<JobCategory> getEntityFactory() {
+		return ENTITY_FACTORY;
+	}
+}
