ef1ed90c6f7b9f6d051d7a7db4e57d7d8b1d266e
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-11-11 01:58:42 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/model/comparator/superclass/AbstractEntitySpecificComparator.java
+++ src/main/java/com/aliuken/jobvacanciesapp/model/comparator/superclass/AbstractEntitySpecificComparator.java
@@ -27,6 +27,7 @@ public abstract class AbstractEntitySpecificComparator<T extends AbstractEntity<
 	public final int compare(final AbstractEntity<T> abstractEntity1, final AbstractEntity<T> abstractEntity2) {
 		final T entity1 = GenericsUtils.cast(abstractEntity1);
 		final T entity2 = GenericsUtils.cast(abstractEntity2);
+
 		final Integer nullCompareResult = this.getNullCompareResult(entity1, entity2);
 		if(nullCompareResult != null) {
 			return nullCompareResult;
