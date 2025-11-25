5126dda05362d5fbbcc352cee17aced0bdaa7fe1
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-11-25 20:15:01 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/util/concurrency/ConcurrencyUtils.java
+++ src/main/java/com/aliuken/jobvacanciesapp/util/concurrency/ConcurrencyUtils.java
@@ -1,5 +1,7 @@
 package com.aliuken.jobvacanciesapp.util.concurrency;
 
+import com.aliuken.jobvacanciesapp.util.javase.FunctionalUtils;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -9,18 +11,26 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class ConcurrencyUtils {
 
-	public static <E> void forEachChunkParallel(final Collection<E> initialElements, final int chunkSize,
-                                                final int numberOfThreads, final Consumer<List<E>> chunkTask) {
+	public static <E> void splitAndRunChunksInParallel(final Collection<E> initialElements, final int chunkSize,
+											   final int numberOfThreads, final Consumer<List<E>> chunkTask) {
 
-		final List<List<E>> chunkList = ConcurrencyUtils.createChunkList(initialElements, chunkSize);
-        ConcurrencyUtils.forEachChunkParallel(chunkList, numberOfThreads, chunkTask);
+		final Function<List<E>, Void> chunkFunction = FunctionalUtils.convertConsumerToFunction(chunkTask);
+		ConcurrencyUtils.splitAndRunChunksInParallel(initialElements, chunkSize, numberOfThreads, chunkFunction);
 	}
 
-    //Create a chunkList from the initialElements with chunkSize as the maximum chuck size
-	public static <E> List<List<E>> createChunkList(final Collection<E> initialElements, final int chunkSize) {
+	public static <E,R> void splitAndRunChunksInParallel(final Collection<E> initialElements, final int chunkSize,
+												 final int numberOfThreads, final Function<List<E>,R> chunkFunction) {
+
+		final List<List<E>> chunkList = ConcurrencyUtils.createChunkList(initialElements, chunkSize);
+		ConcurrencyUtils.runChunksInParallel(chunkList, numberOfThreads, chunkFunction);
+	}
+
+	//Create a chunkList from the initialElements with chunkSize as the maximum chuck size
+	private static <E> List<List<E>> createChunkList(final Collection<E> initialElements, final int chunkSize) {
 		if (initialElements == null || initialElements.isEmpty()) {
 			return Collections.emptyList();
 		}
@@ -43,33 +53,30 @@ public class ConcurrencyUtils {
 		return chunkList;
 	}
 
-    //Execute the chunkTask for every chunk in the chunkList (with the given numberOfThreads)
-    public static <T> void forEachChunkParallel(final List<List<T>> chunkList, final int numberOfThreads,
-                                                final Consumer<List<T>> chunkTask) {
-        if (chunkList.isEmpty()) {
-            return;
-        }
+	//Execute the chunkTask for every chunk in the chunkList (with the given numberOfThreads)
+	private static <E,R> void runChunksInParallel(final List<List<E>> chunkList, final int numberOfThreads,
+												 final Function<List<E>,R> chunkTask) {
+		if (chunkList.isEmpty()) {
+			return;
+		}
 
-        final ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
-        try {
-            final ArrayList<Future<Void>> futures = new ArrayList<>(chunkList.size());
-            for (final List<T> chunk : chunkList) {
-                final Callable<Void> callable = () -> {
-                    chunkTask.accept(chunk);
-                    return null;
-                };
-                final Future<Void> future = executorService.submit(callable);
-                futures.add(future);
-            }
-            for (final Future<Void> future : futures) {
-                try {
-                    future.get();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        } finally {
-            executorService.shutdown();
-        }
-    }
+		final ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
+		try {
+			final ArrayList<Future<R>> futures = new ArrayList<>(chunkList.size());
+			for (final List<E> chunk : chunkList) {
+				final Callable<R> callable = () -> chunkTask.apply(chunk);
+				final Future<R> future = executorService.submit(callable);
+				futures.add(future);
+			}
+			for (final Future<R> future : futures) {
+				try {
+					future.get();
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		} finally {
+			executorService.shutdown();
+		}
+	}
 }
---
+++ src/main/java/com/aliuken/jobvacanciesapp/util/javase/FunctionalUtils.java
@@ -0,0 +1,16 @@
+package com.aliuken.jobvacanciesapp.util.javase;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FunctionalUtils {
+
+    //Converts a consumer to a function returning Void
+    public static <T> Function<T,Void> convertConsumerToFunction(final Consumer<T> consumer) {
+        final Function<T,Void> function = list -> {
+            consumer.accept(list);
+            return null;
+        };
+        return function;
+    }
+}
