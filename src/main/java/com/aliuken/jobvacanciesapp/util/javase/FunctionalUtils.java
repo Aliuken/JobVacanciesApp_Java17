fd73adf07787b69f8c18c6ea2d0440e4c6626293
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-11-25 21:10:17 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/util/javase/FunctionalUtils.java
+++ src/main/java/com/aliuken/jobvacanciesapp/util/javase/FunctionalUtils.java
@@ -1,16 +1,47 @@
 package com.aliuken.jobvacanciesapp.util.javase;
 
+import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class FunctionalUtils {
 
-	//Converts a consumer to a function returning Void
+	//Converts a consumer to a function with Void output
 	public static <T> Function<T,Void> convertConsumerToFunction(final Consumer<T> consumer) {
-		final Function<T,Void> function = list -> {
-			consumer.accept(list);
+		final Function<T,Void> function = input -> {
+			consumer.accept(input);
 			return null;
 		};
 		return function;
+	}
+
+	//Converts a callable to a function with Void input
+	public static <T> Function<Void,T> convertCallableToFunction(Callable<T> callable) {
+		final Function<Void,T> function = input -> {
+			try {
+				return callable.call();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		};
+		return function;
+	}
+
+	//Converts a runnable to a function with Void input and output
+	public static Function<Void,Void> convertRunnableToFunction(Runnable runnable) {
+		final Function<Void,Void> function = input -> {
+			runnable.run();
+			return null;
+		};
+		return function;
+	}
+
+	//Converts a runnable to a callable with Void output
+	public static Callable<Void> convertRunnableToCallable(Runnable runnable) {
+		final Callable<Void> callable = () -> {
+			runnable.run();
+			return null;
+		};
+		return callable;
 	}
 }
