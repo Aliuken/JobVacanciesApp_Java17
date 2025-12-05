547392af1f31d913c4c92b5ad95f3223e902ec37
DESKTOP-36BOFP2\Aliuken <alber84ou@gmail.com>
2025-12-05 09:35:02 +0100

    aaa

--- src/main/java/com/aliuken/jobvacanciesapp/superinterface/Internationalizable.java
+++ src/main/java/com/aliuken/jobvacanciesapp/superinterface/Internationalizable.java
@@ -7,25 +7,25 @@ public interface Internationalizable {
 	public abstract String getMessageName();
 
 	public default String getMessage(final String languageCode) {
-		final String messageName = getMessageName();
+		final String messageName = this.getMessageName();
 		final String message = I18nUtils.getInternationalizedMessage(languageCode, messageName, null);
 		return message;
 	}
 
 	public default String getMessage(final Language language) {
-		final String messageName = getMessageName();
+		final String messageName = this.getMessageName();
 		final String message = I18nUtils.getInternationalizedMessage(language, messageName, null);
 		return message;
 	}
 
 	public default String getMessage(final String languageCode, Object[] messageParameters) {
-		final String messageName = getMessageName();
+		final String messageName = this.getMessageName();
 		final String message = I18nUtils.getInternationalizedMessage(languageCode, messageName, messageParameters);
 		return message;
 	}
 
 	public default String getMessage(final Language language, Object[] messageParameters) {
-		final String messageName = getMessageName();
+		final String messageName = this.getMessageName();
 		final String message = I18nUtils.getInternationalizedMessage(language, messageName, messageParameters);
 		return message;
 	}
